b73a6cd2085057ec43a927b5069937e29a73ce40
Vincent-Férié <arshey@live.fr>
2019-11-09 09:43:14 +0000

    final

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 $page = "home";
-<<<<<<< HEAD
-=======
 require_once('inc/config.php');
->>>>>>> feature-1
 include('inc/header.php');
 include('inc/nav.php');
 
@@ -70,10 +67,6 @@ $req2 = $req->fetchAll();
     }
 
 ?>
-<<<<<<< HEAD
-
-    <div class="container mt-5">
-=======
 
  
     <div class="container mt-5">
@@ -98,23 +91,16 @@ $req2 = $req->fetchAll();
 	?>
 
 
->>>>>>> feature-1
         <div class="jumbotron">
         <h1 class="display-4">Home</h1>
         <hr class="my-4">
         <p class="lead">
     <div class="login-form">
-<<<<<<< HEAD
-        <form action="" method="POST" name="frmAdd" class="ml-5 text-center" enctype="multipart/form-data">
-            <h2 class="text-center">Ajoutes des informations</h2>
-
-=======
 
        
         <form action="" method="POST" name="frmAdd" class="ml-5 text-center" enctype="multipart/form-data">
             <h2 class="text-center">Ajouter des informations</h2>
 
->>>>>>> feature-1
             <hr class="w-25 bg-dark">
 
             <div class="form-group ">
@@ -124,73 +110,13 @@ $req2 = $req->fetchAll();
                 <input type="text" name="prenom" class="form-control w-25" placeholder="Prenom">
             </div>
             <div class="form-group">
-<<<<<<< HEAD
-                <input type="file" name="image" class="form-control" id="file" > 
-=======
                 <input type="file" name="image" class="form-control w-50" id="file" > 
->>>>>>> feature-1
             </div>
 
             
                 <input type="submit" name="submit" class="btn btn-primary">
-<<<<<<< HEAD
-           
-
-        </form>
-    </div>
-        </p>
-    </div>
-    </div>
-=======
->>>>>>> feature-1
 
 
-<<<<<<< HEAD
-    error_reporting( ~E_NOTICE );
-
-    if (isset($_POST['submit'])) {
-
-        $prenom = htmlspecialchars($_POST['prenom']);
-        $nom = htmlspecialchars($_POST['nom']);
-
-        $imgFile = $_FILES['image']['name'];
-		$tmp_dir = $_FILES['image']['tmp_name'];
-        $imgSize = $_FILES['image']['size'];
-        
-        if(empty($imgFile)){
-			$errMSG = "Please Select Image File.";
-		} else {
-			$upload_dir = 'assets/uploads/'; 
-	
-			$imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); 
-		
-			
-			$valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); 
-		
-			
-			$image = rand(1000,1000000).".".$imgExt;
-				
-			
-			if(in_array($imgExt, $valid_extensions)){			
-				
-				if($imgSize < 5000000)				{
-                    move_uploaded_file($tmp_dir,$upload_dir.$image);
-                    echo "Wesh c bon";
-				}
-				else{
-					$errMSG = "Désolé l'image est un peu trop grande.";
-				}
-			}
-			else{
-				$errMSG = "Désolé seule les format 'jpeg', 'jpg', 'png', 'gif' sont autorisés";		
-			}
-		}
-       
-        echo "Bonjour, votre nom c'est ".$nom. " et votre prenom c'est ". $prenom;
-    }
-
-?>
-=======
 
 <div class="container ">
     <div class="container mt-5">
@@ -242,16 +168,10 @@ $req2 = $req->fetchAll();
 
         
             </table>
->>>>>>> feature-1
 
         </p>
     </div>
 </div>
 
-<<<<<<< HEAD
-</body>
-</html>
-=======
 <?php
 include('inc/footer.php');
->>>>>>> feature-1
